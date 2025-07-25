152385655c78bd1e6ebb4aec0b7c60c5b0fbb456
RenoMahardika-ai <renomahardika0@gmail.com>
2025-07-25 09:24:07 +0700

    Galeri jogja

---
+++ google_tagmanager.php
@@ -0,0 +1,15 @@
+<script data-ad-client="ca-pub-8745395778565707" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 703587434 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-703587434"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-703587434');
+</script>
+<!-- Event snippet for Kunjungan halaman conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-703587434/7Ox2CL2Eu8cBEOrIv88C'});
+</script>
